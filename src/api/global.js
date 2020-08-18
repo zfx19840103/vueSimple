52e49f8b4f2f72b7c88dc0a054e49ec2d8cef743
zfx15652559172 <zfx15652559172@126.com>
2020-08-18 13:29:18 +0800

    add

--- src/api/global.js
+++ src/api/global.js
@@ -1,3 +1,4 @@
 // const API = '//apitest.tsingglobal.com';
-const API = 'http://dev.api.byte.tsingglobal.cn';
+// const API = 'http://dev.api.byte.tsingglobal.cn';
+const API = 'http://tsingtest.test.tsingglobal.cn';
 export default API;
