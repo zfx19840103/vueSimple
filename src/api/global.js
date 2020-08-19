431839dc647a1d801dd352859cada6bac54fd6d0
zfx15652559172 <zfx15652559172@126.com>
2020-08-19 16:19:12 +0800

    add

--- src/api/global.js
+++ src/api/global.js
@@ -1,8 +1,9 @@
-const API = '//tsingapi.test.tsingglobal.cn';
+// const API = '//tsingapi.test.tsingglobal.cn';
 // const API = 'http://dev.api.byte.tsingglobal.cn';
+const API = '//apitest.tsingglobal.com';
 // const API = 'http://tsingtest.test.tsingglobal.cn';
 // 联调环境：dev.api.byte.tsingglobal.cn
 // 测试环境：apitest.tsingglobal.com
 // 线上环境：tsingapi.tsingglobal.com
 
-export default API;
+export default API
