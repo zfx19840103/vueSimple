d3611cd8cdc67924c119814bb61e05e65e79da3c
zfx15652559172 <zfx15652559172@126.com>
2020-08-30 10:44:53 +0800

    add

--- config/index.js
+++ config/index.js
@@ -54,7 +54,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'https://test-maf-h5-static.tsingglobal.com',
 
     /**
      * Source Maps
